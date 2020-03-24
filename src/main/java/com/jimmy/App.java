3c6cb40ac4c441a8b52e0290dd6008c487ed0449
Jimmy Thinkpad <jimmy@thinkpad.com>
2020-03-24 20:39:47 +0200

    hibernate setup with mysql5

---
+++ src/main/java/com/jimmy/App.java
@@ -0,0 +1,42 @@
+package com.jimmy;
+
+/**
+ * Hello world!
+ *
+ */
+import java.util.List;
+
+
+import com.jimmy.Entities.Employee;
+import com.jimmy.Entities.Department;
+import com.jimmy.Util.*;
+
+import org.hibernate.*;
+
+public class App {
+
+    public static void main(String[] args) {
+
+        Session session = HibernateUtil.getSessionFactory().openSession();
+
+        session.beginTransaction();
+
+        Department department = new Department("java");
+        session.save(department);
+
+        session.save(new Employee("Jakab Gipsz",department));
+        session.save(new Employee("Captain Nemo",department));
+
+        session.getTransaction().commit();
+
+        Query q = session.createQuery("From Employee ");
+
+        List<Employee> resultList = q.list();
+        System.out.println("num of employess:" + resultList.size());
+        for (Employee next : resultList) {
+            System.out.println("next employee: " + next);
+        }
+
+    }
+
+}
