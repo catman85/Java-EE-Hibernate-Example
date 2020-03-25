8a7e62633ef0e443138f8f847862bb26bb5fa079
Jimmy Thinkpad <jimmy@thinkpad.com>
2020-03-25 17:30:02 +0200

    removed .idea and updated main

--- src/main/java/com/jimmy/App.java
+++ src/main/java/com/jimmy/App.java
@@ -19,28 +19,28 @@ public class App {
     public static void main(String[] args) {
 
         Session session = HibernateUtil.getSessionFactory().openSession();
-
         session.beginTransaction();
-
+        //---TRANSACTION--//
         Department department = new Department("java");
         session.save(department);
-
         session.save(new Employee("Jakab Gipsz", department));
         session.save(new Employee("Captain Nemo", department));
 
-        session.getTransaction().commit();
-
         Query q = session.createQuery("From Employee ");
         List<Employee> resultList = q.list();
+        System.out.println("num of employess:" + resultList.size());
 
         Employee e = (Employee) session.get(Employee.class, (long) 2);
-        System.out.printf("First Employees name: %s\n", e.getName());
+        System.out.printf("Second Employees name: %s\n", e.getName());
 
+        session.delete(e);
 
-        System.out.println("num of employess:" + resultList.size());
         for (Employee next : resultList) {
             System.out.println("next employee: " + next.toString());
         }
+
+        //---END---//
+        session.getTransaction().commit();
         session.close();
         HibernateUtil.shutdown();
     }
